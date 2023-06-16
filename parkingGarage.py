280d4b24758e07e08349c80d7c7bed8b659ebb77
Nickero Dean <nickerodean@Nickeros-MacBook-Air.local>
2023-06-16 16:42:15 -0700

    Second session, Nick Driving

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,32 +1,39 @@
 class Parking_garage():
 
-    def __init__(self,parkingSpaces, currentTicket):
-
+    def __init__(self,parkingSpaces):
         self.parkingSpaces = parkingSpaces
         self.tickets = parkingSpaces
-        self.currentTicket = currentTicket
+        self.currentTicket = 1
+        self.ticketStatus = dict()
     
     def print_info(self):
-        print(f'This parking garage has {self.parkingSpaces} space and {self.tickets} tickets.')
+        print(f'{self} parking garage has {self.parkingSpaces} space and {self.tickets} tickets.')
 
     def takeTicket(self):
         self.parkingSpaces -= 1
         self.tickets -= 1
+        print(f"Your ticket number is: {self.currentTicket}")
+        self.ticketStatus[self.currentTicket] = False
+        self.currentTicket += 1
 
     def payForParking(self,currentTicket):
-        ans_pay_for_parking = input('Would you like to pay for your ticket? Yes/No: ')
-        if ans_pay_for_parking.lower() == "yes" or ans_pay_for_parking == "y":
-            print("Thank you for your payment. You have 15 minuntes to ")
-            pass
-        elif ans_pay_for_parking.lower() == "no" or ans_pay_for_parking.lower() == "n":
-            pass
-        else:
-            print("That is not a valid response. Please write 'yes' or 'no' to indicate if you'd like to pay for your ticket")
+        while True:
+            ticketNumber = int(input('Please insert your ticket number: '))
+            if ticketNumber in self.ticketStatus:
+                self.ticketStatus[ticketNumber] = True
+                print("Thank you for your payment. You have 15 minuntes to exit")
+                
+            elif ticketNumber.lower() == "no" or ticketNumber.lower() == "n":
+                pass
+            else:
+                print("That is not a valid response. Please write 'yes' or 'no' to indicate if you'd like to pay for your ticket")
 
     
     # def payForParking(self):
 
-ChicagoParkingGarage = Parking_garage(200,1)
+ChicagoParkingGarage = Parking_garage(200)
 ChicagoParkingGarage.print_info()
 ChicagoParkingGarage.takeTicket()
 ChicagoParkingGarage.print_info()
+ChicagoParkingGarage.takeTicket()
+print(ChicagoParkingGarage.ticketStatus)
